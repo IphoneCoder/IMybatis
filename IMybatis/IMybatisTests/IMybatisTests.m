47dd5a4176883d06ab85740c2fa955557accf58b
littleCoder <1445440736@qq.com>
2014-12-15 10:57:18 +0800

    增加单元测试
    1.增加参数是string,返回结果是单个对象的单元测试
    2.增加参数是string,返回结果是多个对象的单元测试
    3.增加参数是map,返回结果是单个对象
    4.增加参数是object,返回结果是单个对象
    5.增加参数是int,返回结果是单个对象,使用resultMap映射
    6.增加参数是int,返回结果是多个对象,使用resultMap映射

--- IMybatis/IMybatisTests/IMybatisTests.m
+++ IMybatis/IMybatisTests/IMybatisTests.m
@@ -56,7 +56,7 @@
     NSLog(@"%@",config);
     
 }
--(void)testSelectOneUserWithInt
+-(void)testSelectOneUserWithInt //参数是int
 {
     id<SqlSession> session =[self getSqlSession];
     //映射 sql 的标识字符串
@@ -65,21 +65,68 @@
     UserBean *user = [session selectOne:statement parameter:@2];
     NSLog(@"=====%@",[user description]);
 }
--(void)testSelectOneUserWithString
+-(void)testSelectOneUserWithString //参数是string,返回结果是单个对象
 {
+    
    id<SqlSession> session =[self getSqlSession];
     NSString *statement =@"com.mybatis.userMapper.getUserWithString";
-   
+   //如果调用selectOne 但是有多条记录，会抛出异常
     UserBean *user = [session selectOne:statement parameter:@"Tom"];
     NSLog(@"=====%@",[user description]);
 }
--(void)testSelectListUserWithString
+-(void)testSelectListUserWithString //参数是string,返回结果是多个对象
 {
     id<SqlSession> session =[self getSqlSession];
     NSString *statement =@"com.mybatis.userMapper.getUserWithString";
     
-    UserBean *user = [session selectList:statement parameter:@"Tom"];
-    NSLog(@"=====%@",[user description]);
+   NSArray *userArray= [session selectList:statement parameter:@"Tom"];
+    NSLog(@"=====%lu",(unsigned long)userArray.count);
 }
 
+
+-(void)testSelectOneUserWithDictionary //参数是map,返回结果是单个对象
+{
+   id<SqlSession> session =[self getSqlSession];
+    NSString *statement =@"com.mybatis.userMapper.getUserWithMap";
+    //执行查询返回一个唯一 user 对象的 sql
+    UserBean *user = [session selectOne:statement parameter:[NSDictionary dictionaryWithObject:@1 forKey:@"id"]];
+    NSLog(@"=====%@",[user description]);
+    
+}
+//省略 参数是map,返回结果是多个对象得测试，直接调用 session selectList即可
+
+
+
+-(void)testSelectOneUserWithObject //参数是object,返回结果是单个对象
+{
+   id<SqlSession> session =[self getSqlSession];
+    NSString *statement =@"com.mybatis.userMapper.getUserWithObject";
+    //执行查询返回一个唯一 user 对象的 sql
+    UserBean *userBean=[[UserBean alloc]init];
+    userBean.userID=2;
+    UserBean *user = [session selectOne:statement parameter:userBean];
+    NSLog(@"=====%@",[user description]);
+    
+}
+//省略 参数是object,返回结果是多个对象得测试，直接调用 session selectList即可
+
+-(void)testSelectOneUserWithResultMap //参数是int,返回结果是单个对象,使用resultMap映射
+{
+    id<SqlSession> session =[self getSqlSession];
+    NSString *statement =@"com.mybatis.userMapper.getOneUserResultMap";
+    //执行查询返回一个唯一 user 对象的 sql
+    UserBean *userBean = [session selectOne:statement parameter:@2];
+    NSLog(@"=====%@",[userBean description]);
+    
+}
+-(void)testSelectListUserWithResultMap //参数是int,返回结果是多个对象,使用resultMap映射
+{
+   id<SqlSession> session =[self getSqlSession];
+    NSString *statement =@"com.mybatis.userMapper.getListUserResultMap";
+    //执行查询返回一个唯一 user 对象的 sql
+    NSArray *userBeanArray = [session selectList:statement parameter:@"Tom"];
+    for (UserBean *userBean in userBeanArray) {
+        NSLog(@"=====%@",[userBean description]);
+    }
+}
 @end
